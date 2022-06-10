fa49f064474215ddb9113d2eebec7eea05de6237
jej12w4h1294h12iknsanbdauisnd <105560833+jej12w4h1294h12iknsanbdauisnd@users.noreply.github.com>
2022-06-10 20:23:16 -0300

    Delete scripts directory

--- scripts/mobile.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('mobile-menu').addEventListener('click', function() {
-  document.getElementById('menu-popup').classList.toggle('hidden');
-});
-
-function closePopup() {
-  document.getElementById('menu-popup').classList.add('hidden');
-}

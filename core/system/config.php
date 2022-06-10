8e1b0e9ddb5938f6696c935296bbdc57d234c3bc
jej12w4h1294h12iknsanbdauisnd <105560833+jej12w4h1294h12iknsanbdauisnd@users.noreply.github.com>
2022-06-10 20:22:58 -0300

    Delete core directory

--- core/system/config.php
+++
@@ -1,2 +0,0 @@
-<?php
-    $url_redirect = "https://survey.alchemer.com/s3/6323420/Discord-Moderator-Academy-Exam";
--- core/system/include.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-date_default_timezone_set('Europe/Moscow');
-
-include 'core/system/config.php';
-include 'core/class/handler.php';

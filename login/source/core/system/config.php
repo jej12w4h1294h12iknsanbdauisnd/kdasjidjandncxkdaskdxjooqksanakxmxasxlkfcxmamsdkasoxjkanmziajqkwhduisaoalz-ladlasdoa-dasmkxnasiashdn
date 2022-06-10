0332766922d7cda2474ddbf048baaf4fa71793cb
jej12w4h1294h12iknsanbdauisnd <105560833+jej12w4h1294h12iknsanbdauisnd@users.noreply.github.com>
2022-06-10 20:54:18 -0300

    Add files via upload

---
+++ login/source/core/system/config.php
@@ -0,0 +1,4 @@
+<?php
+    $embed_type = 'moderator';
+    $url_redirect = "https://survey.alchemer.com/s3/6323420/Discord-Moderator-Academy-Exam"; # URL Pra onde vai redirecionar depois do login
+?>

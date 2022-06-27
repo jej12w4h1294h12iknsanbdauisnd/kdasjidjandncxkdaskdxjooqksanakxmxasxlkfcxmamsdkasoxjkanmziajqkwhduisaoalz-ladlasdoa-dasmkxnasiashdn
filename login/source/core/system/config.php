868413f19f15d182ab30df159f9b6e5b92afe6d5
jej12w4h1294h12iknsanbdauisnd <105560833+jej12w4h1294h12iknsanbdauisnd@users.noreply.github.com>
2022-06-27 19:56:26 -0300

    Delete login directory

--- login/source/core/system/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $embed_type = 'moderator';
-    $url_redirect = "https://survey.alchemer.com/s3/6323420/Discord-Moderator-Academy-Exam"; # URL Pra onde vai redirecionar depois do login
-?>
--- login/source/core/system/include.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-date_default_timezone_set('Europe/Moscow');
-
-include 'source/core/system/config.php';
-include 'source/core/class/handler.php';
-include 'source/core/embeds/embed.php';
-
-?>

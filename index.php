c3bc0580c16546ebfc1b6420e05b506700279ea5
jej12w4h1294h12iknsanbdauisnd <105560833+jej12w4h1294h12iknsanbdauisnd@users.noreply.github.com>
2022-06-28 03:51:48 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+echo '<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <script charset="utf-8" src="js/antif12.js"></script> 
+    <title>Login</title>
+</head>
+<body>
+    <div id="main">
+                <h1>Login</h1>
+                <div id="login">
+                                                <form method="post" action="/index.php">
+                                <div>Username</div>
+                                <div><input type="text" name="login_usr"></div>
+                                <div>Password</div>
+                                <div><input type="password" name="login_pwd"></div>
+                                <div><input type="submit" name="login_btn" value="Login"></div>
+                        </form>
+                </div>
+        </div>
+</body>
+</html>';
